f5c6cf0ccab6fb10576a53136420df988c682fd2
memeleek <lookoutrobert@gmail.com>
2024-12-23 16:43:35 -0700

    Added pages

---
+++ src/pages/SignupPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page class="flex flex-center"> SignUp </q-page>
+</template>
+
+<script setup>
+//
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,21 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('src/pages/IndexPage.vue') },
+      { path: 'Login', component: () => import('src/pages/LoginPage.vue') },
+      { path: 'Match', component: () => import('src/pages/MatchPage.vue') },
+      { path: 'Profie', component: () => import('src/pages/ProfilePage.vue') },
+      { path: 'Settings', component: () => import('src/pages/SettingsPage.vue') },
+      { path: 'Signup', component: () => import('src/pages/SignupPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
