02e3831a66ae16cc280b51c7461adbc86df166f7
memeleek <lookoutrobert@gmail.com>
2025-01-21 13:49:41 -0700

    index page formatting

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,11 @@
 <template>
-  <q-page class="flex flex-top">
+  <q-page class="flex flex-center">
     <h1>Welcome to shuttle Buddy</h1>
-    <q-btn to="login" label="Login" outline color="purple" />
-    <h4>Dont Have an account? <q-btn to="Signup" label="SignUp" outline color="purple" /></h4>
+    <h4 display:block>
+      <div class="q-row"><q-btn to="login" label="Login" outline color="purple" /></div>
+      <br />
+      Dont Have an account? <q-btn to="Signup" label="SignUp" outline color="purple" />
+    </h4>
   </q-page>
 </template>
 
